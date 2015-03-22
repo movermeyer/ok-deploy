0e43c3d5c9d689f1d587b276b75bc55956da6abc
jhermann <jh@web.de>
2015-03-22 14:08:12 +0100

    :new: imported project template from py-generic-project 1.2dev

---
+++ tasks.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=wildcard-import, unused-wildcard-import, bad-continuation
+""" Project automation for Invoke.
+"""
+from __future__ import absolute_import, unicode_literals
+
+from invoke import run, task
+from rituals.invoke_tasks import * # pylint: disable=redefined-builtin
+
+
+@task
+def ci(): # pylint: disable=invalid-name
+    """Perform continuous integration tasks."""
+    run("invoke clean --all build --docs test check --reports 2>&1")
